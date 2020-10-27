6dd3b3d9a7c5938dab0ce5f2602b4409267f366c
PercyMgn <61540735+PercyMgn@users.noreply.github.com>
2020-10-27 16:18:10 +0100

    modification json.py
    ajout frequency, channel, rssi et timeStamp_trame

--- lib/json.py
+++ lib/json.py
@@ -20,13 +20,16 @@ from lib.sh.sh import service
 
 # Librairies natives Python
 import json
+import time
 
 from multiprocessing import queues
 from struct import *
 
 
 # ## FONCTIONS ## #
-
+list_wifi = {2412:1, 2417:2, 2422:3, 2427:4, 2432:5, 2437:6, 2442:7,
+                    2447:8, 2452:9, 2457:10, 2462:11, 2467:12, 2472:13, 2484:14
+                    }
 
 def verif_and_construction_json(queue_beacon_sie):
     """
@@ -45,6 +48,11 @@ def verif_and_construction_json(queue_beacon_sie):
         
         dataDict = {}
         data = trame[scapy.Dot11EltVendorSpecific].info[1:]
+
+        frequency = trame.Channel
+        channel = list_wifi[frequency]
+        rssi = trame.dBm_AntSignal
+        timeStamp_trame = int(trame.time)
         
         try:
             if vs_type == vs_protocole and len(str(trame[scapy.Dot11EltVendorSpecific].info)) > 30: # taille minimale de la trame
@@ -54,60 +62,68 @@ def verif_and_construction_json(queue_beacon_sie):
                 v = ""
 
                 for i in data:
-                    try:
-                        t = ord(data[0:1])
-                        l = ord(data[1:2])
-                        hopl = 2 + l
+                    if len(str(data)) > 3:
 
-                    except TypeError as err:
-                        break
+                        try:
+                            t = ord(data[0:1])
+                            l = ord(data[1:2])
+                            hopl = 2 + l
 
-                    try:
-                        if t == 2 or t == 3:
-                            """
-                                Conversion bytes vers string.
-                                Type 2 pour l'identifiant FR sur 30 caractères.
-                                Type 3 pour l'identifiant ANSI.
-                            """
-                            v = str(data[2:hopl], "utf-8")
-                            data = data[hopl:]
+                        except TypeError as err:
+                            print("erreur ", err)
+                            break
 
-                        elif t == 4 or t == 5 or t == 8 or t == 9:
-                            """
-                                Conversion bytes vers float avec présicion de 5 chiffres apres la virgule.
-                                On récupère la data en Bytes on la passe en int signé.
-                                type 4 et 5 pour latitude et longitude.
-                                type 8 et 9 pour latitude de départ et longitude de départ.
-                            """
-                            v = float(int.from_bytes(data[2:hopl], byteorder="big", signed=True))/100000
-                            data = data[hopl:]
+                        try:
+                            if t == 2 or t == 3:
+                                """
+                                    Conversion bytes vers string.
+                                    Type 2 pour l'identifiant FR sur 30 caractères.
+                                    Type 3 pour l'identifiant ANSI.
+                                """
+                                v = str(data[2:hopl], "utf-8")
+                                data = data[hopl:]
 
-                        elif t == 6 or t == 7:
-                            """
-                                Conversion bytes vers int.
-                                On récupère la data en Bytes on la passe en int signé
-                                Type 6 et 7 pour l'altitude et hauteur courante.
-                            """
-                            v = int.from_bytes(data[2:hopl], byteorder="big", signed=True)
-                            data = data[hopl:]
+                            elif t == 4 or t == 5 or t == 8 or t == 9:
+                                """
+                                    Conversion bytes vers float avec présicion de 5 chiffres apres la virgule.
+                                    On récupère la data en Bytes on la passe en int signé.
+                                    type 4 et 5 pour latitude et longitude.
+                                    type 8 et 9 pour latitude de départ et longitude de départ.
+                                """
+                                v = float(int.from_bytes(data[2:hopl], byteorder="big", signed=True))/100000
+                                data = data[hopl:]
 
-                        elif t == 10 or t == 11 or t == 1:
-                            """
-                                Conversion bytes vers int.
-                                On récupère la data en Bytes on la passe en int  non signé
-                                Type 10 et 11 pour la vitesse et la direction en degrés.
-                            """
-                            v = int.from_bytes(data[2:hopl], byteorder="big", signed=False)
-                            data = data[hopl:]
+                            elif t == 6 or t == 7:
+                                """
+                                    Conversion bytes vers int.
+                                    On récupère la data en Bytes on la passe en int signé
+                                    Type 6 et 7 pour l'altitude et hauteur courante.
+                                """
+                                v = int.from_bytes(data[2:hopl], byteorder="big", signed=True)
+                                data = data[hopl:]
 
-                    except TypeError as err:
-                        print(err)
-                        break
+                            elif t == 10 or t == 11 or t == 1:
+                                """
+                                    Conversion bytes vers int.
+                                    On récupère la data en Bytes on la passe en int  non signé
+                                    Type 10 et 11 pour la vitesse et la direction en degrés.
+                                """
+                                v = int.from_bytes(data[2:hopl], byteorder="big", signed=False)
+                                data = data[hopl:]
 
-                    """
-                        Ajout des informations dans un dictionnaire.
-                    """
-                    dataDict.update({t : v})
+                        except TypeError as err:
+                            print("erreur 2 ", err)
+                            break
+
+                        """
+                            Ajout des informations dans un dictionnaire.
+                        """
+                        dataDict.update({t : v})
+
+                dataDict.update({201: timeStamp_trame})
+                dataDict.update({202: int(time.time())})
+                dataDict.update({203: channel})
+                dataDict.update({204: rssi})
                 
                 # création du JSON grâce au dictionnaire
                 dataJSON = json.dumps(dataDict, separators=(',', ':'))
